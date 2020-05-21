14cad52cd14f3fa2adf5e5baf3e91deb3b20574c
telmovaz <40360576+telmovaz@users.noreply.github.com>
2020-05-21 15:50:07 +0200

    task8 simple redux

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -2,14 +2,13 @@ import React from "react";
 import "./Sidebar.scss";
 import btnimage from "../../utils/more.png";
 import AddHotel from "../AddHotel/AddHotel";
-import axios from "axios";
 import preloader from "../../utils/Spin-1s-200px.gif";
 import Modal from "../Modal/Modal";
+import { connect } from 'react-redux';
 
 class Sidebar extends React.Component {
   state = {
-    sidebarApi: [],
-    show: false
+    
   };
 
   showModal = () => {
@@ -20,28 +19,29 @@ class Sidebar extends React.Component {
   }
 
   componentDidMount() {
-    axios
-      .get("https://nodejs-mysql-it-academy.herokuapp.com/hotels/recommended")
-      .then((res) => {
-        let preloader = document.querySelector("#preloader");
-        preloader.parentNode.removeChild(preloader);
-        this.setState({
-          sidebarApi: res.data,
-        });
-      });
+    // axios
+    //   .get("https://nodejs-mysql-it-academy.herokuapp.com/hotels/recommended")
+    //   .then((res) => {
+    //     let preloader = document.querySelector("#preloader");
+    //     preloader.parentNode.removeChild(preloader);
+    //     this.setState({
+    //       sidebarApi: res.data,
+    //     });
+    //   });
+    
   }
 
   render() {
     return (
       <div className="Sidebar">
-        <div id="preloader">
-          <img src={preloader} alt="preloader" />
-        </div>
+       
         <h1>More than just hotels</h1>
 
-        {this.state.sidebarApi.map((element, index) => {
+        {this.props.hotels.map((element, index) => {
+          
           return (
-            <div key={index}>
+            <div key={index} className = {element.recommended ? "display-hotel" : "not-display-hotel"}>
+              
               <div className="sidebar-wrapper">
                 <div className="sidebar-thumb">
                   <img src={element.image} alt="" />
@@ -71,4 +71,10 @@ class Sidebar extends React.Component {
   }
 }
 
-export default Sidebar;
+const mapStateToProps = (state) => {
+  return {
+    hotels: state.hotels
+  }
+}
+
+export default connect(mapStateToProps)(Sidebar);
