6487b0660b4db5530a3472fa0ed9a86f8569b55b
telmovaz <40360576+telmovaz@users.noreply.github.com>
2020-06-13 12:22:18 +0200

    new build

---
+++ docs/precache-manifest.42a6e16d85a89b7b3a0d065785ac2c0f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea50d0058d3d4dbf2799c0e7013d48a8",
+    "url": "/travel-app/index.html"
+  },
+  {
+    "revision": "5f47d152c75b6cbcca76",
+    "url": "/travel-app/static/css/main.21851cf4.chunk.css"
+  },
+  {
+    "revision": "1a4ba8d1d2e2de749f19",
+    "url": "/travel-app/static/js/2.03625279.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/travel-app/static/js/2.03625279.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f47d152c75b6cbcca76",
+    "url": "/travel-app/static/js/main.7c7c5941.chunk.js"
+  },
+  {
+    "revision": "3b034883bed8941eeba8",
+    "url": "/travel-app/static/js/runtime-main.d37d0c9b.js"
+  },
+  {
+    "revision": "2320281bcf999616b7169977b307a2d2",
+    "url": "/travel-app/static/media/heart-filled.2320281b.svg"
+  },
+  {
+    "revision": "5e30bffdb50ba8359f7e574899a0ed21",
+    "url": "/travel-app/static/media/heart.5e30bffd.svg"
+  },
+  {
+    "revision": "c412776699c042548bfc30e7b2fa85ca",
+    "url": "/travel-app/static/media/search.c4127766.svg"
+  }
+]);
